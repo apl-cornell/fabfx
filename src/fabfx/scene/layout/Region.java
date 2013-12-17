fd295016434f220bde7e5e1b8ba75cdf09bbb786
Yizhou Zhang <yizhou@cs.cornell.edu>
2013-12-17 18:26:34 -0500

    trying to make the hello example urn

--- src/fabfx/scene/layout/Region.java
+++ src/fabfx/scene/layout/Region.java
@@ -2,11 +2,13 @@ package fabfx.scene.layout;
 
 import fabfx.scene.Parent;
 import fabric.lang.security.Label;
+import javafx.scene.layout.Pane;
 
 public class Region extends Parent {
 
     public Region (Label L, Label M) {
-	super(L, M, new javafx.scene.layout.Region());
+		super(L, M, null);//new javafx.scene.layout.Region()); 
+		Pane p = new Pane();
     }
 
 }
