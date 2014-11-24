93b8e6139ec341f23f63df690fdf1ada60373581
Yizhou Zhang <yizhou@cs.cornell.edu>
2014-11-24 17:37:26 -0500

    A hello-world example. Still requiring Fabric handling varargs in order for this small example to work.

--- src/fabfx/application/Application.java
+++ src/fabfx/application/Application.java
@@ -3,6 +3,9 @@ package fabfx.application;
 import fabric.lang.security.Label;
 
 abstract public class Application extends javafx.application.Application {
+	
+	public void fabfx$application$Application() {}
+
 	abstract public void run(fabfx.stage.Stage primaryStage);
 
 	@Override
