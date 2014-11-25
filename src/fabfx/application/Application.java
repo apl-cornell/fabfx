ffc88710723109a2ba7d892c0a1a1e957976cee7
Yizhou Zhang <yizhou@cs.cornell.edu>
2014-11-25 14:31:19 -0500

    circumvented the launching problem

--- src/fabfx/application/Application.java
+++ src/fabfx/application/Application.java
@@ -1,10 +1,13 @@
 package fabfx.application;
 
+import com.sun.javafx.application.LauncherImpl;
+
 import fabric.lang.security.Label;
 
 abstract public class Application extends javafx.application.Application {
-	
-	public void fabfx$application$Application() {}
+
+	public void fabfx$application$Application$() {
+	}
 
 	abstract public void run(fabfx.stage.Stage primaryStage);
 
@@ -12,6 +15,10 @@ abstract public class Application extends javafx.application.Application {
 	public void start(javafx.stage.Stage jstage) {
 		fabfx.stage.Stage fstage = new fabfx.stage.Stage(jstage);
 		this.run(fstage);
+	}
+
+	public void launchApp() {
+		LauncherImpl.launchApplication(this.getClass(), new String[0]);
 	}
 
 }
