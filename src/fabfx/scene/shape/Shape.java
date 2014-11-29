6f56fe38eb9a912e7d269a072da57f39364b85e8
Yizhou Zhang <yizhou@cs.cornell.edu>
2014-11-29 17:18:25 -0500

    Tiles.

---
+++ src/fabfx/scene/shape/Shape.java
@@ -0,0 +1,30 @@
+package fabfx.scene.shape;
+
+import javafx.scene.paint.Paint;
+import fabfx.scene.Node;
+import fabric.lang.security.Label;
+
+public abstract class Shape extends Node {
+
+	public Shape(Label L, Label M) {
+		super(L, M);
+	}
+
+	@Override
+	public javafx.scene.shape.Shape _impl() {
+		return (javafx.scene.shape.Shape) super._impl();
+	}
+
+	public final void setFill(Paint value) {
+		_impl().setFill(value);
+	}
+
+	public final void setStrokeWidth(double value) {
+		_impl().setStrokeWidth(value);
+	}
+
+	public final void setStroke(Paint value) {
+		_impl().setStroke(value);
+	}
+
+}
